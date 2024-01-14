283a808ab3daeb91ac7f251079942116a2dcd826
swaroopaborra444 <155412238+swaroopaborra444@users.noreply.github.com>
2024-01-14 15:14:24 +0530

    Update Ticket.java

--- java/src/main/java/Ticket.java
+++ java/src/main/java/Ticket.java
@@ -1,1 +1,12 @@
+public class Ticket {
+    private String from;
+    private String to;
+    private String userFirstName;
+    private String userLastName;
+    private String userEmail;
+    private int price;
+    private String section;
+    private String seat;
 
+    // Constructor, getters, and setters
+}
